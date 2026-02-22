ea7bdaa346dbef5ecd969d93e1eb6e1449af5b27
LT-Nacl <139832384+LT-Nacl@users.noreply.github.com>
2026-02-22 17:55:16 -0500

    Data + basic potential 3d map with heatmap

---
+++ ltitievsky60_EthanLeoEfield (1).py
@@ -0,0 +1,31 @@
+from vpython import *
+#Web VPython 3.2
+
+def csv_parse(fd):
+    items = []
+    text = fd.text
+    for line in iter(text.splitlines()):
+        if len(line.strip()) > 1 :
+            items.append(line.strip().split(','))
+    return items
+            
+        
+def grid(values):
+    L = len(values)
+    H = len(values[1])
+    for i in range(L):
+        for j in range(H):
+            box(
+                length = 4, 
+                height = 4, 
+                pos = vector(4*j-(2*H), 4*i-(2*L), int(values[i][j])), 
+                color = vec((1/6)*int(values[i][j]),0,0)
+                )
+
+    
+f = read_local_file(scene.title_anchor)
+voltages = csv_parse(f)
+
+grid(voltages)
+
+ 
